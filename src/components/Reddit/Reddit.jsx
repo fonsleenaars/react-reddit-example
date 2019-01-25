fbc9c068f3dc43910d5426f9f4d2982b39f02500
Fons Leenaars <fonsleenaars@gmail.com>
2019-01-25 12:26:25 -0500

    feat: add fetch from reddit
    To get things started, let's add a polyfill for fetch and just run
    the fetch command to see what we get back, store it in the component
    state and display the number of posts.
    
    To continue development after this, it's time to add some tests that
    mock the data so we can begin asserting what our component should
    show.

--- src/components/Reddit/Reddit.jsx
+++ src/components/Reddit/Reddit.jsx
@@ -1,11 +1,32 @@
+import 'whatwg-fetch';
 import React from 'react';
 
 
 export default class Reddit extends React.Component {
+  state = {
+    posts: [],
+  };
+
+  componentDidMount() {
+    window.fetch('https://www.reddit.com/r/pics.json')
+      .then(response => response.json())
+      .then((json) => {
+        this.setState({
+          posts: json.data.children,
+        });
+      });
+  }
+
   render() {
+    const {
+      posts,
+    } = this.state;
+
     return (
       <div>
-        Sup reddit!
+        There are
+        {` ${posts.length} `}
+        posts from reddit.
       </div>
     );
   }
