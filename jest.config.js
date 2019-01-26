eede199d9599dcd5681fff3a4016cfe066034275
Fons Leenaars <fonsleenaars@gmail.com>
2019-01-26 17:20:48 -0500

    test: fix jest config

--- jest.config.js
+++ jest.config.js
@@ -3,11 +3,9 @@ module.exports = {
   transform: {
     // Make sure we're transpiling JS/JSX
     '^.+\\.(js|jsx)$': 'babel-jest',
-    // Cover CSS module errors;
-    '.*': '<rootDir>/node_modules/jest-css-modules',
-  },
-  moduleNameMapper: {
     // Mock static assets:
-    '\\.(jpg|jpeg|png|gif|eot|otf|svg|ttf|woff|woff2)$': '<rootDir>/__mocks__/fileTransformer.js',
+    '^.+\\.(jpg|jpeg|png|gif|eot|otf|svg|ttf|woff|woff2)$': '<rootDir>/__mocks__/fileTransformer.js',
+    // Lastly cover CSS module errors
+    '^.+\\.css$': '<rootDir>/node_modules/jest-css-modules',
   },
 };
